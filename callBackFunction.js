2ad3816d75446fdf3590da7e6825ccef51f955c5
Victor Jong <victor.jong@ncs.com.sg>
2023-11-24 21:16:00 +0800

    callBackFunction

--- callBackFunction.js
+++ callBackFunction.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates the result of a callback function and doubles it.
+ * @param {number} num1 - The first number.
+ * @param {number} num2 - The second number.
+ * @param {function} calc - The callback function to perform the calculation.
+ * @returns {number} The doubled result of the callback function.
+ */
 function doubleResult(num1, num2, calc) {
     return calc(num1, num2) * 2;
 }
@@ -12,3 +19,28 @@ function multiply(num1, num2) {
   
 console.log(doubleResult(4, 2, add));
 console.log(doubleResult(4, 2, multiply));
+
+
+/**
+ * Creates a new array by applying a callback function to each element of the input array.
+ * @param {Array} arr - The input array.
+ * @param {Function} callback - The callback function to be applied to each element of the input array.
+ * @returns {Array} - The new array created by applying the callback function to each element of the input array.
+ */
+function createNewArray(arr, callback) {
+const output = [];
+for (let i = 0; i < arr.length; i++) {
+    output.push(callback(arr[i]));
+};
+return output;
+}
+
+function double(input) { return input * 2; };
+function divide(input) { return input / 2; };
+function increment(input) { return input + 1 };
+
+const array = [1, 2, 3];
+
+console.log(createNewArray(array, double));
+console.log(createNewArray(array, divide));
+console.log(createNewArray(array, increment));
