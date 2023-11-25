e275cb8b091908a606d1d5eb0acdbd8cff7bb3e4
Victor Jong <victor.jong@ncs.com.sg>
2023-11-25 13:21:09 +0800

    commonMistakes

--- commonMistakes.js
+++ commonMistakes.js
@@ -1,8 +1,19 @@
 const {checkInventory, processPayment, shipOrder} = require('./library.js');
 
+/**
+ * Represents an order with items and gift card balance.
+ * @typedef {Object} Order
+ * @property {Array<Array<string, number>>} items - The items in the order, each represented as an array with the item name and quantity.
+ * @property {number} giftcardBalance - The balance of the gift card used for the order.
+ */
+
+/**
+ * The order details.
+ * @type {Order}
+ */
 const order = {
-  items: [['sunglasses', 1], ['bags', 2]],
-  giftcardBalance: 79.82
+    items: [['sunglasses', 1], ['bags', 2]],
+    giftcardBalance: 79.82
 };
 
 // Refactor the code below:
