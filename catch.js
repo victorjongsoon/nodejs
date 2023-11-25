e23f696e11b34adf19bee539ba0d79920fd58569
Victor Jong <victor.jong@ncs.com.sg>
2023-11-25 13:05:08 +0800

    catch.js

---
+++ catch.js
@@ -0,0 +1,22 @@
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+/**
+ * Handles the success case by logging the resolved value.
+ * @param {any} resolvedValue - The value resolved by the promise.
+ * @returns {void}
+ */
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+    console.log(rejectReason);
+};
+
+// Write your code below:
+// checkInventory(order).then(handleSuccess, handleFailure);
+checkInventory(order)
+    .then(handleSuccess)
+    .catch(handleFailure);
