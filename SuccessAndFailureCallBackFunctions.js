21950443a000e912b0766913323dbdfa247fdd72
Victor Jong <victor.jong@ncs.com.sg>
2023-11-25 12:49:59 +0800

    successAndFailureCallBackFunctions

--- SuccessAndFailureCallBackFunctions.js
+++ SuccessAndFailureCallBackFunctions.js
@@ -3,7 +3,16 @@ const {checkInventory} = require('./library.js');
 const order = [['sunglasses', 1], ['bags', 2]];
 
 // Write your code below:
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
 
+/**
+ * Handles the failure of a callback function.
+ * @param {string} rejectionReason - The reason for the rejection.
+ */
+const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+};
 
-
-
+checkInventory(order).then(handleSuccess, handleFailure);
--- library.js
+++ library.js
@@ -4,6 +4,11 @@ const inventory = {
     bags: 1344
   };
   
+/**
+ * Checks the inventory to determine if the items in the order are available.
+ * @param {Array<Array<string, number>>} order - The order containing items and their quantities.
+ * @returns {Promise<string>} - A promise that resolves to a success message if the order can be fulfilled, or rejects with an error message if some items are sold out.
+ */
   const checkInventory = (order) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
