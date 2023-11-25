7440ef52fb122bc863a6c96b0c0e2e65448379b3
Victor Jong <victor.jong@ncs.com.sg>
2023-11-26 03:00:21 +0800

    independent promises

--- async-await/independentPromises.js
+++ async-await/independentPromises.js
@@ -1,6 +1,10 @@
 let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library4.js');
 
 // Write your code below:
+/**
+ * Asynchronously serves dinner by preparing and cooking various components.
+ * @returns {Promise<void>} A promise that resolves when dinner is served.
+ */
 async function serveDinner(){
   let vegetablePromise = steamBroccoli();
   let starchPromise = cookRice();
