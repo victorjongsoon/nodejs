cd9eb5765b96f6bc99d77c468a6d19512f923117
Victor Jong <victor.jong@ncs.com.sg>
2023-11-25 19:39:03 +0800

    celsius to fahrenheit

--- celsius-to-fahrenheit.js
+++ celsius-to-fahrenheit.js
@@ -1,4 +1,9 @@
 /* celsius-to-fahrenheit.js */
+/**
+ * Converts Celsius to Fahrenheit.
+ * @param {number} celsius - The temperature in Celsius.
+ * @returns {number} The temperature in Fahrenheit.
+ */
 function celsiusToFahrenheit(celsius) {
     return celsius * (9/5) + 32;
 }
@@ -7,3 +12,6 @@ const celsiusInput = process.argv[2]; // Get the 3rd input from the argument lis
 const fahrenheitValue = celsiusToFahrenheit(celsiusInput);
 
 console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue} degrees Fahrenheit`);
+
+// run in command 
+// celsius-to-fahrenheit 100
