33f2f9a3c6ca5ac83865f92017569fe1c329c291
Victor Jong <victor.jong@ncs.com.sg>
2023-11-30 00:06:04 +0800

    codecademy mvc react expense tracker

---
+++ mvc-architecture/mvc/starting-code/routes/expense.js
@@ -0,0 +1,18 @@
+// require express and it's router component
+const express = require('express');
+
+const router = express.Router();
+
+// require the middlewares and callback functions from the controller directory
+
+// Create POST route to create an expense
+
+// Create GET route to read an expense
+
+// Create PUT route to update an expense
+
+// Create DELETE route to remove an expense
+
+// Create GET route to read a list of expenses
+
+module.exports = router;
